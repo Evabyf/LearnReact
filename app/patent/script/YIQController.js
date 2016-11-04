901fb90460e9198a7f90bd1fbf3e116d68749707
470222649 <470222649@qq.com>
2016-11-04 11:28:39 +0800

    删除

--- app/patent/script/YIQController.js
+++
@@ -1,3 +0,0 @@
-angular.module('chatapp').controller('YIQController',[function () {
-
-}])
--- app/patent/script/YKQController.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by yudragon on 2016/10/31.
- */
-angular.module('chatapp').controller('YKQController',[function () {
-
-}])
